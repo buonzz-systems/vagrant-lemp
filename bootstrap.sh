d772e820eeed254171f464b73ec149bc964d9db8
buonzz <buonzz@gmail.com>
2015-01-18 05:11:57 +0800

    simple

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-sudo apt-get install -y git-core
+apt-get update
+
+apt-get upgrade -y
+
+apt-get install nginx php5-fpm php5-cli php5-mcrypt git -y
+apt-get install mysql-server -y
