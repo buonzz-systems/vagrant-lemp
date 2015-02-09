0193fe857739de5f4382f1b11ca940c1af1a0d0b
buonzz <buonzz@gmail.com>
2015-02-09 11:24:52 +0800

    installed curl, php mongodb driver and composer

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,20 @@
 
 apt-get update
 
+# install mcrypt
 sudo apt-get install -y php5-mcrypt
 sudo php5enmod mcrypt
 
-apt-get install -y php5-cli git
+# install console and git
+apt-get install -y php5-cli git curl
+
+# install mongodb
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo apt-get install -y php5-mongo
+
+# Install Composer
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
